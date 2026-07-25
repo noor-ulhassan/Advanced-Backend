1217e270ae76c457f5f543d06ad77b06d551af2a
Noor <thenoorulhassan.dev@gmail.com>
2026-07-26 04:48:14 +0500

    Made api that uses Redis Successfully

--- level2/phase1/index.js
+++ level2/phase1/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
 import { User } from "./models/user.model.js";
+import { Redis } from "ioredis";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+const redis = new Redis(process.env.REDIS_URL);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -17,6 +21,7 @@ app.get("/", (req, res) => {
 app.post("/create", async (req, res) => {
   const { name, email, password } = req.body;
   const user = await User.create({ name, email, password });
+  await redis.del("user:all");
 
   if (!user) {
     return res.status(400).json({ message: "User not created" });
@@ -29,6 +34,19 @@ app.post("/create", async (req, res) => {
 
 app.get("/get", async (req, res) => {
   const user = await User.find();
+  return res.json(user);
+});
+
+// Get api with Redis
+
+app.get("/get-with-redis", async (req, res) => {
+  const cached = await redis.get("user:all");
+  if (cached) {
+    const user = JSON.parse(cached);
+    return res.json(user);
+  }
+  const user = await User.find({});
+  await redis.set("user:all", JSON.stringify(user));
   return res.json(user);
 });
 
