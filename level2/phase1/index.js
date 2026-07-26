f70bd21a02a6c0f7a567f7d7bcb64d294faba2de
Noor <thenoorulhassan.dev@gmail.com>
2026-07-27 00:19:15 +0500

    Added Rate Limiter

--- level2/phase1/index.js
+++ level2/phase1/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
 import { User } from "./models/user.model.js";
 import { Redis } from "ioredis";
+import rateLimiter from "./middleware/ratelimit.js";
 
 dotenv.config();
 
@@ -10,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-const redis = new Redis(process.env.REDIS_URL);
+export const redis = new Redis(process.env.REDIS_URL);
 
 app.use(express.json());
 
@@ -32,7 +33,7 @@ app.post("/create", async (req, res) => {
 
 // Get data
 
-app.get("/get", async (req, res) => {
+app.get("/get", rateLimiter, async (req, res) => {
   const user = await User.find();
   return res.json(user);
 });
---
+++ level2/phase1/middleware/ratelimit.js
@@ -0,0 +1,18 @@
+import { redis } from "../index.js";
+
+const rateLimiter = async (req, res, next) => {
+  const ip = req.ip;
+  const key = `rate_limit:${ip}`;
+  const requests = await redis.incr(key);
+  if (requests == 1) {
+    await redis.expire(key, 60);
+  }
+  if (requests > 5) {
+    return res
+      .status(429)
+      .json({ success: false, message: "Too many requests" });
+  }
+  next();
+};
+
+export default rateLimiter;
